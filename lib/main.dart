614e112b5936cec4b8c7636787fa754373b34840
soniyav20 <soniyanw@nativewit.in>
2023-10-27 22:06:08 +0530

    Firebase Issue Solved Finally

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:prod_dev_23/views/login_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyA-oUvLl18GsT01XsUPLF-UlW6LP3v0cEg",
+          appId: '1:12862108178:android:3b2a2b055e9e69dc965ecb',
+          messagingSenderId: '12862108178',
+          projectId: 'proddev2023'));
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(MaterialApp(
